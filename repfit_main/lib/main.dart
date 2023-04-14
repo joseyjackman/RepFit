c696bb6307bbaebb9bc93653fb412eb4c5946730
joseyjackman <joseyjackman@gmail.com>
2023-04-14 15:52:51 -0400

    merged raymond's code with mine (everything above exercisedatabase class was replaced)

--- repfit_main/lib/main.dart
+++ repfit_main/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 
 //charts package:
 import 'package:syncfusion_flutter_charts/charts.dart';
+//import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 // ignore_for_file: prefer_const_constructors
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
@@ -11,12 +12,12 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 import 'user_data.dart';
 //import 'package:path_provider/path_provider.dart';
 import 'dart:io';
-//import 'package:hive/hive.dart';
+import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 //data storage
 
-Future main() async {
+void main() async {
   await Hive.initFlutter();
   runApp(MyApp());
 }
@@ -163,7 +164,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       Expanded(
                         child: Image.asset(
                           'assets/images/logo.png',
-                          fit: BoxFit.cover,
+                          fit: BoxFit.fill,
                         ),
                       ),
                     if (selectedIndex == 0) SizedBox(height: 16),
@@ -223,18 +224,140 @@ class BigCard extends StatelessWidget {
 }
 
 class TutorialPage extends StatelessWidget {
+  //begin modifications to add a youtube player:
+  //video ids below:
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    Widget intro = Container(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text(
+                      'INTRO',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+                Text(
+                    'Welcome to the RepFit fitness app! This page will show you exactly what your options are to navigate this app, as well as what each option means. Aside from this Tutorial page, there are 4 other pages: The Home page, the Exercise Database, the History page, and the Start Session page.')
+              ],
+            ))
+          ],
+        ));
+
+    Widget home = Container(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text(
+                      'HOME',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+                Text(
+                    'The function of the Home page is to serve as an introduction to the rest of the RepFit app. There, you will find an introduction to RepFit\'s founders, as well as our mission statement.')
+              ],
+            ))
+          ],
+        ));
+
+    Widget exercisedatabase = Container(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text(
+                      'EXERCISE DATABASE',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+                Text(
+                    'The Exercise Database your one-stop shop for all your exercise questions. Conatining both videos and a text description of every exercise we track in the RepFit app, the database allows you to either begin new forms of exercise, or else brush up on your existing technique.')
+              ],
+            ))
+          ],
+        ));
+
+    Widget history = Container(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text(
+                      'HISTORY',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+                Text(
+                    'In order to view your fitness progress, head over to the History page. As you complete more exercise sessions, the graph will increase in detail, allowing you to see your exact rate of improvement for each exercise.')
+              ],
+            ))
+          ],
+        ));
+
+    Widget startsession = Container(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text(
+                      'START SESSION',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+                Text(
+                    'Last, but certainly not least, is the Start Session page, where you\'ll actually record your progress each time you work out. After navigating to the Start Session page, you\'ll be able to select the exercise you\'d like to do. After completing the exercise, simply enter the number of repetitions, and voila! The number will be automatically stored in the History tab, to be viewed next time you\'re over there.')
+              ],
+            ))
+          ],
+        ));
+
+    /*if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }*/
 
     return ListView(
       children: [
-        Center(child: new Text('Squats: ')),
-        Image.asset('assets/database_vids/squat.gif'),
-        Center(child: new Text('Push-Ups:')),
-        Image.asset('assets/database_vids/pushup.gif'),
-        Center(child: new Text('Sit-Ups: ')),
-        Image.asset('assets/database_vids/situp.gif'),
+        intro,
+        home,
+        exercisedatabase,
+        history,
+        startsession,
       ],
     );
   }
