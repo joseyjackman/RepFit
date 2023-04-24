e10fdc5bba39f5c35128fe0d09d9cfe5b11e6938
Dracolightning08 <125089303+Dracolightning08@users.noreply.github.com>
2023-04-24 14:37:35 -0400

    Updated Timer to show Hours and operate in Hr time

--- repfit_main/lib/main.dart
+++ repfit_main/lib/main.dart
@@ -688,9 +688,10 @@ class _StartSessionPageState extends State<StartSessionPage> {
   -----------------------------------------------------------------------------------------*/
 
   String _formatTime(int seconds) {
-    final minutes = (seconds ~/ 60).toString().padLeft(2, '0');
+    final hours = (seconds ~/ 3600).toString().padLeft(2, '0');
+    final minutes = ((seconds ~/ 60) % 60).toString().padLeft(2, '0');
     final remainingSeconds = (seconds % 60).toString().padLeft(2, '0');
-    return '$minutes:$remainingSeconds';
+    return '$hours:$minutes:$remainingSeconds';
   }
 
   /*-----------------------------------------------------------------------------------------
