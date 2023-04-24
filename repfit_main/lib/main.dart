bb3be3d87165b6705f9b2d48667422ab4117b29c
Dracolightning08 <125089303+Dracolightning08@users.noreply.github.com>
2023-04-24 14:46:58 -0400

    HunterCode+Raymond+AustinMASTER

--- repfit_main/lib/main.dart
+++ repfit_main/lib/main.dart
@@ -170,7 +170,42 @@ class _MyHomePageState extends State<MyHomePage> {
                     if (selectedIndex == 0)
                       Expanded(
                         child: Column(
-                          children: [Image.asset('assets/images/logo.png')],
+                          children: [
+                            Image.asset(
+                              'assets/images/logo.png',
+                              height: 200,
+                              fit: BoxFit.fitHeight,
+                            ),
+                            Container(
+                                padding:
+                                    const EdgeInsets.only(top: 40, bottom: 40),
+                                child: const Text(
+                                  'MISSION STATEMENT',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )),
+                            Text(
+                              'Our mission is to allow users to develop a synergy with our app, creating a positive feedback loop in which the user will calendarise progress via gamification, leading to a more robust lifestyle.',
+                              textAlign: TextAlign.center,
+                            ),
+                            Image.asset(
+                              'assets/images/whiteboys.png',
+                              height: 200,
+                              fit: BoxFit.fitHeight,
+                            ),
+                            Container(
+                                padding:
+                                    const EdgeInsets.only(top: 40, bottom: 40),
+                                child: const Text(
+                                  'ABOUT THE FOUNDERS',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )),
+                            Text(
+                                'RepFit was founded by Hunter Odom, Austin Jackman, and Raymond Riddell, three young Computer Science majors with a passion for both exercise and helping others. Filler text filler text filler text filler text.')
+                          ],
                         ),
                       ),
                     if (selectedIndex == 0) SizedBox(height: 1),
